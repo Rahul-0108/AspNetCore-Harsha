0666cb8ff3b5ef09e3e949d5cd3947965880271d
Rahul-0108 <53996840+Rahul-0108@users.noreply.github.com>
2026-08-04 10:37:02 +0530

    Update Program.cs

--- 20. Filters/08. IOrderedFilter/CRUDExample/Program.cs
+++ 20. Filters/08. IOrderedFilter/CRUDExample/Program.cs
@@ -20,11 +20,10 @@ builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, 
 
 //it adds controllers and views as services
 builder.Services.AddControllersWithViews(options => {
- //options.Filters.Add<ResponseHeaderActionFilter>(5);
 
  var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
 
- options.Filters.Add(new ResponseHeaderActionFilter(logger, "My-Key-From-Global", "My-Value-From-Global", 2));
+ options.Filters.Add(new ResponseHeaderActionFilter(logger, "My-Key-From-Global", "My-Value-From-Global", 2)); // set order
 });
 
 //add services into IoC container

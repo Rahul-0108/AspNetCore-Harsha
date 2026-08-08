2ca27c7a0caef55ffe1b2a9a617e623ddfb0b5c5
Rahul-0108 <53996840+Rahul-0108@users.noreply.github.com>
2026-08-08 18:02:52 +0530

    Update PersonAlwaysRunResultFilter.cs

--- 20. Filters/16. IAlwaysRunResultFilter/CRUDExample/Filters/ResultFilters/PersonAlwaysRunResultFilter.cs
+++ 20. Filters/16. IAlwaysRunResultFilter/CRUDExample/Filters/ResultFilters/PersonAlwaysRunResultFilter.cs
@@ -2,6 +2,7 @@
 
 namespace CRUDExample.Filters.ResultFilters
 {
+// for IAlwaysRunResultFilter both OnResultExecuting and   OnResultExecuted always executed even if previous filters are short circuited
  public class PersonAlwaysRunResultFilter : IAlwaysRunResultFilter
  {
   public void OnResultExecuted(ResultExecutedContext context)
